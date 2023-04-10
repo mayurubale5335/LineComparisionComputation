eccb837cf04e97b34673c58773b8849e4e0d14b1
unknown <mayurubale5335@gmail.com>
2023-04-10 21:55:05 +0530

    [mayur ubale] Modify Calculate the Length

--- LineComparisionComputation/Program.cs
+++ LineComparisionComputation/Program.cs
@@ -10,6 +10,19 @@ namespace LineComparisionComputation
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Welcome to Line Comparision Problem");
+
+            // UC1-LineOfLength
+
+            Console.WriteLine("Enter the value of x1 and y1");
+            int x1 = Convert.ToInt32(Console.ReadLine());
+            int y1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter the value of x2 and y2");
+            int x2 = Convert.ToInt32(Console.ReadLine());
+            int y2 = Convert.ToInt32(Console.ReadLine());
+
+            double length = Math.Sqrt((x2 - x1) ^ +(y2 - y1) ^ 2);
+            Console.WriteLine("Line of length is: " + length);
         }
     }
 }
